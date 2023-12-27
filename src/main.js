2e8d5960114f1e71903a5e8dc63115fbc8bfa4ff
Antoine from SolarEclipse <hoangtuan11102@gmail.com>
2023-12-27 05:11:46 -0500

    Highlight the entire score :) Analysis tool coming soon

--- src/main.js
+++ src/main.js
@@ -28,11 +28,15 @@ class NeumeComponentAQ extends NeumeComponent {
     super(id, tilt);
     this.loc = loc;
   }
+
+  highlight() {
+    super.highlight();
+  }
 }
 
-function parse_MEI(MEI_file_path) {
+async function parse_MEI(MEI_file_path) {
   let mei_array = [];
-  fetch(MEI_file_path)
+  await fetch(MEI_file_path)
     .then((res) => res.text())
     .then((mei) => {
       // Parse the XML .mei file to mutable JS type
@@ -62,14 +66,19 @@ export function load_search() {
   document.getElementById('search-output').innerHTML = searchField;
 }
 
-function highlight_neume_component(nc_id) {
-  let neume = document.getElementById(nc_id);
-
-  let neume_nc = neume.querySelectorAll('g.nc');
-  for (let nc of neume_nc) {
-    // console.log(nc);
-    nc.style.fill = 'red';
-  }
+/**
+ * 
+ * @param {Array<Number>} search_AQ an array of number, parse from user's input
+ */
+function highlight_nc_to_search_AQ(MEI_file_path, search_AQ) {
+  const search_AQ_hardcode = [1, 2, 1]; // temporary hardcode
+  parse_MEI(MEI_file_path)
+    .then((res) => res)
+    .then((nc_arr) => {
+      for (const nc of nc_arr) {
+        nc.highlight();
+      }
+    });
 }
 
 /**
@@ -81,8 +90,6 @@ function load_MEI_file(file_name, order) {
   fetch(file_name)
     .then((response) => response.text())
     .then((mei) => {
-      // get the ID from mei to get the <loc> 
-      // console.log(mei);
       // This line initializes the Verovio toolkit
       const vero_toolkit = new verovio.toolkit();
 
@@ -93,30 +100,12 @@ function load_MEI_file(file_name, order) {
         scale: zoom,
       };
       vero_toolkit.setOptions(options);
-
-      /*
-      Use the mei to get the position via 'loc' attr
-      Based on loc, use the xml:id from the mei to highlight SVG
-      (mei xml:id == svg id) 
-      */
       vero_toolkit.loadData(mei);
       let svg = vero_toolkit.renderToSVG(1);
       const meifile = document.getElementById("mei-file-" + order);
 
       meifile.innerHTML = svg;
-      // console.log(meifile)
-      highlight_neume_component('m-3f92be62-a103-11ee-b763-f2b2e3f0b573');
-      // let neume = document.querySelectorAll('g.nc');
-
-      /*
-      Store all notes in a list with their ID, for example
-      [{
-        note
-        id
-      }]
-  
-      */
-
+      // highlight_neume_component('m-3f92be62-a103-11ee-b763-f2b2e3f0b573');
     }
     )
 };
@@ -128,6 +117,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // load_MEI_file('/GABCtoMEI/MEI_outfiles/01_benedicte-omnes_pem82441_aquit_AQUIT.mei', 1);
   // load_MEI_file('/GABCtoMEI/MEI_outfiles/02_benedicte-omnes_pem85041_square_SQUARE.mei', 2);
   load_MEI_file('http://server.notkaramel.me/~antoine/01_benedicte-omnes_pem82441_aquit_AQUIT.mei', 1);
-  parse_MEI('http://server.notkaramel.me/~antoine/01_benedicte-omnes_pem82441_aquit_AQUIT.mei')
+  highlight_nc_to_search_AQ('http://server.notkaramel.me/~antoine/01_benedicte-omnes_pem82441_aquit_AQUIT.mei')
   load_MEI_file('http://server.notkaramel.me/~antoine/02_benedicte-omnes_pem85041_square_SQUARE.mei', 2);
 });
