da9057a7e1ce20d01bcd3b51255d3ba7f74ecd2f
Antoine from SolarEclipse <hoangtuan11102@gmail.com>
2023-12-28 15:33:49 -0500

    Add sample chants to build assets

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { highlight_nc_to_search_SQ, highlight_nc_to_search_AQ, load_MEI_file } from './utils.js';
 
-const AQUIT_SAMPLE = "http://server.notkaramel.me/~antoine/01_benedicte-omnes_pem82441_aquit_AQUIT.mei";
-const SQUARE_SAMPLE = 'http://server.notkaramel.me/~antoine/02_benedicte-omnes_pem85041_square_SQUARE.mei'
+import AQUIT_SAMPLE from '../GABCtoMEI/MEI_outfiles/01_benedicte-omnes_pem82441_aquit_AQUIT.mei?url'
+import SQUARE_SAMPLE from '../GABCtoMEI/MEI_outfiles/02_benedicte-omnes_pem85041_square_SQUARE.mei?url'
+
 document.getElementById('search-btn').addEventListener("click", load_search, false)
 
 function load_search() {
@@ -12,8 +13,6 @@ function load_search() {
  * Load predefined files when DOM is loaded
  */
 document.addEventListener("DOMContentLoaded", () => {
-  // load_MEI_file('/GABCtoMEI/MEI_outfiles/01_benedicte-omnes_pem82441_aquit_AQUIT.mei', 1);
-  // load_MEI_file('/GABCtoMEI/MEI_outfiles/02_benedicte-omnes_pem85041_square_SQUARE.mei', 2);
   load_MEI_file(AQUIT_SAMPLE, 1);
   load_MEI_file(SQUARE_SAMPLE, 2);
 });
