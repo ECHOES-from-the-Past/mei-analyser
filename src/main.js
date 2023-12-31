d74a544e4c9dd18b8759b6f3c37bfa3932117c4d
Antoine from SolarEclipse <hoangtuan11102@gmail.com>
2023-12-30 20:09:27 -0500

    refactor some codes, added documentation, and added search selection

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { highlight_nc_to_search_SQ, highlight_nc_to_search_AQ, load_MEI_file } from './utils.js';
+import { highlight_contour_square, highlight_absolute, load_MEI_file, parse_search_pattern,   clear_all_highlight } from './utils.js';
 
 import AQUIT_SAMPLE from '../GABCtoMEI/MEI_outfiles/01_benedicte-omnes_pem82441_aquit_AQUIT.mei?url'
 import SQUARE_SAMPLE from '../GABCtoMEI/MEI_outfiles/02_benedicte-omnes_pem85041_square_SQUARE.mei?url'
@@ -6,7 +6,20 @@ import SQUARE_SAMPLE from '../GABCtoMEI/MEI_outfiles/02_benedicte-omnes_pem85041
 document.getElementById('search-btn').addEventListener("click", load_search, false)
 
 function load_search() {
-  highlight_nc_to_search_AQ(AQUIT_SAMPLE)
+  clear_all_highlight();
+  const form = document.querySelector("#search-form");
+  const form_data = new FormData(form);
+
+  const search_option = form_data.get("search-option");
+  const search_pattern = form_data.get("search-bar");
+  console.log(search_option)
+  if(search_option == "absolute") {
+    let user_input = parse_search_pattern(search_pattern);
+    highlight_absolute(AQUIT_SAMPLE, user_input);
+  } else if (search_option == "contour-square") {
+    highlight_contour_square(AQUIT_SAMPLE);
+  }
+
 }
 
 /**
--- src/utils.js
+++ src/utils.js
@@ -78,9 +78,16 @@ export function load_MEI_file(file_name, order) {
     )
 };
 
-export function parse_user_input() {
-  const user_input = document.getElementById('search-bar').value;
-  return user_input.match(/-?\d/g).map(Number);
+/**
+ * Parse user's input on the search bar via Regular Expression (Regex) to return an array of numbers.
+ * Regex pattern: `/-?\d/g`
+ * - an optional negative `-` sign
+ * - a single digit
+ * @param {FormData} search_pattern 
+ * @returns {Array<Number>} an array of type number from user's input
+ */
+export function parse_search_pattern(search_pattern) {
+  return search_pattern.match(/-?\d/g).map(Number);
 }
 
 export function clear_all_highlight() {
@@ -92,27 +99,26 @@ export function clear_all_highlight() {
 }
 
 /**
-* 
-* @param {Array<Number>} search_AQ an array of number, parse from user's input
-*/
-export function highlight_nc_to_search_AQ(MEI_file_path) {
-  clear_all_highlight();
-  const search_array = parse_user_input(); 
+ * A function that highlight Aquitanian chant based on its absolute location (`@loc` attribute in the MEI file)
+ * @param {file} MEI_file_path the aquitanian MEI (.mei) file to be highlighted
+ * @param {Array<Number>} search_pattern an array of number, parse from user's input
+ */
+export function highlight_absolute(MEI_file_path, search_pattern) {
   parse_MEI_AQ(MEI_file_path)
     .then((res) => res)
     .then((nc_arr) => {
       let search_count = 0;
-      // console.log(nc_arr);
+
       /**
        * nc is type NeumeComponentAQ
        * @param {NeumeComponentAQ[]} nc_arr
        */
       for (let i_nc = 0; i_nc < nc_arr.length; i_nc++) {
-        if(nc_arr[i_nc].loc == search_array[0]) {
+        if (nc_arr[i_nc].loc == search_pattern[0]) {
           let i_search = 1;
           let search_found = [nc_arr[i_nc]];
-          while (i_search < search_array.length) {
-            if(nc_arr[i_nc + i_search].loc == search_array[i_search]) {
+          while (i_search < search_pattern.length) {
+            if (nc_arr[i_nc + i_search].loc == search_pattern[i_search]) {
               search_found.push(nc_arr[i_nc + i_search]);
               i_search++;
             } else {
@@ -120,8 +126,8 @@ export function highlight_nc_to_search_AQ(MEI_file_path) {
               search_found = [];
               break;
             }
-          } 
-          
+          }
+
           // highlight the search found
           for (const nc of search_found) {
             nc.highlight();
@@ -136,16 +142,54 @@ export function highlight_nc_to_search_AQ(MEI_file_path) {
 }
 
 /**
+ * A function that highlight Aquitanian chant based on its contour location.
+ * It also search the Square chant for a similar contour pattern.
  * 
- * @param {Array<Number>} search_SQ an array of number, parse from user's input
+ * The seach output may overlap; hence, it needs a pattern record and should allow
+ * users to visit each pattern at a time.
+ * @param {file} Aquitanian_Chant the chant's MEI file in Aquitanian notation
+ * @param {file} Square_Chant the chant's MEI file in square notation 
+ * @param {Array<Number>} search_pattern an array of number, parse from user's input
  */
-export function highlight_nc_to_search_SQ(MEI_file_path, search_SQ) {
-  const search_AQ_hardcode = [1, 2, 1]; // temporary hardcode
-  parse_MEI_SQ(MEI_file_path)
-    .then((res) => res)
-    .then((nc_arr) => {
-      for (const nc of nc_arr) {
-        nc.highlight();
+export function highlight_contour_square(Aquitanian_Chant, Square_Chant, search_pattern) {
+  parse_MEI_AQ(Aquitanian_Chant)
+  .then((res) => res)
+  .then((nc_arr) => {
+    let search_count = 0;
+    /**
+     * nc is type NeumeComponentAQ
+     * @param {NeumeComponentAQ[]} nc_arr
+     */
+    for (let i_nc = 0; i_nc < nc_arr.length; i_nc++) {
+      if (nc_arr[i_nc].loc == search_pattern[0]) {
+        let i_search = 1;
+        let search_found = [nc_arr[i_nc]];
+        while (i_search < search_pattern.length) {
+          if (nc_arr[i_nc + i_search].loc == search_pattern[i_search]) {
+            search_found.push(nc_arr[i_nc + i_search]);
+            i_search++;
+          } else {
+            // Reset list if no search found
+            search_found = [];
+            break;
+          }
+        }
+
+        // highlight the search found
+        for (const nc of search_found) {
+          nc.highlight();
+        }
+        search_count++;
+
       }
-    });
+    }
+
+    document.getElementById("search-count").innerHTML = search_count;
+  });
+
+  parse_MEI_SQ(Square_Chant)
+  .then((res) => res)
+  .then(() => {
+    console.log("hehe");
+  })
 }
