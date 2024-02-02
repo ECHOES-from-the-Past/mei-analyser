cab1f1ba91584dd15120562c41bbe52c25340f02
Antoine Phan <antoine.phan@mail.mcgill.ca>
2024-02-02 12:24:22 -0500

    Filter out non-.mei files

--- updateDatabase.cjs
+++ updateDatabase.cjs
@@ -1,5 +1,17 @@
 const fs = require('fs');
 
 let allMEIfiles = fs.readdirSync('./GABCtoMEI/MEI_outfiles');
+
+// Filter out only MEI files
+let i = 0;
+while (i < allMEIfiles.length) {
+    if (!allMEIfiles[i].endsWith('.mei')) {
+        allMEIfiles.splice(i, 1);
+    } else {
+        i++;
+    }
+}
+
+console.table(allMEIfiles);
 // Write all files/folder to database.json (a JSON list)
 fs.writeFileSync('./src/database.json', JSON.stringify(allMEIfiles), 'utf8');
