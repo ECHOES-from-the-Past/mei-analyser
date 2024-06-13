8bc502ceaa3ea3278b1353cb72244efc70190b6e
Antoine Phan <antoine.phan@mail.mcgill.ca>
2024-06-13 10:39:18 -0400

    Segmenting smith waterman test

--- src/cross-comparison/test-algorithm.js
+++ src/cross-comparison/test-algorithm.js
@@ -85,15 +85,31 @@ function nw_test05() {
   console.log(align_nw(M, A, B, gapSymbol));
 }
 
+function sw_test06() {
+  const A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 3, 5, 2, 1, 5, 3, 4, 5, 6, 7, 8, 9];
+  const B = [7, 8, 9]; // search query
+  const match = 1, mismatch = -1, gap = -1;
+
+  for(let i = 0; i < A.length - B.length; i++) {
+    let A_toCompare = A.slice(i, i + B.length);
+    console.log("A_toCompare: ", A_toCompare)
+    const M = matrix_sw(A_toCompare, B, match, mismatch, gap);
+    console.table(M);
+    console.log(align_sw(M, A, B, match, mismatch, gap));
+  }
+}
+
 function main() {
   // nw_test01();
   // nw_test02();
   // nw_test03();
   // nw_test04();
-  sw_test01();
-  sw_test02();
-  sw_test03();
-  nw_test05();
+  // sw_test01();
+  // sw_test02();
+  // sw_test03();
+  // nw_test05();
+
+  sw_test06();
 }
 
 main();
