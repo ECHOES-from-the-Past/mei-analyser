90016a54d2af2af1a439245100f56acc1cf16fdd
Antoine Phan <antoine.phan@mail.mcgill.ca>
2024-07-31 14:49:26 +0700

    Sample chart

--- src/panels/ExperimentalPanel.svelte
+++ src/panels/ExperimentalPanel.svelte
@@ -7,7 +7,9 @@
     import { Chant } from "../utility/components";
     import { getNeumeComponentList } from "../utility/components";
 
-    let testChart;
+    class ChartDataset {}
+
+    let testChart1, testChart2;
 
     const databaseURL =
         env == "development" ? "src/database/database.json" : "./database.json";
@@ -21,7 +23,7 @@
         const testChant = resultChantList[0];
 
         let notes = getNeumeComponentList(testChant.syllables);
-        console.log(notes);
+        // console.log(notes);
 
         const pitchFrequency = notes.map((nc) => {
             if (testChant.notationType == "aquitanian") return nc.loc;
@@ -37,32 +39,86 @@
             }
         });
 
-        let chantData = [];
-        for (let i in counts) {
-            chantData.push({
-                x: `loc ${i}`,
-                y: counts[i],
-            });
+        const finalis = notes[notes.length - 1];
+
+        const rhumbus = notes.filter((nc) => nc.tilt == "se")
+        console.log(rhumbus)
+
+        const normalBg = "rgba(255, 99, 132, 0.2)",
+            finalisBg = "rgba(75, 192, 192, 0.2)";
+        const normalBorder = "rgb(255, 99, 132)",
+            finalisBorder = "rgb(75, 192, 192)";
+
+        let chantData = {
+            labels: [],
+            datasets: [
+                {
+                    data: [],
+                    backgroundColor: [],
+                    borderColor: [],
+                    borderWidth: 1,
+                },
+                {
+                    data: [],
+                    backgroundColor: [],
+                    borderColor: [],
+                    borderWidth: 1,
+                },
+            ],
+        };
+
+        for (let key in counts) {
+            chantData.datasets[0].data.push(counts[key]);
+
+            if (key == finalis.loc) {
+                chantData.labels.push(`${key} (finalis)`);
+                chantData.datasets[0].backgroundColor.push(finalisBg);
+                chantData.datasets[0].borderColor.push(finalisBorder);
+            } else {
+                chantData.labels.push(key);
+                chantData.datasets[0].backgroundColor.push(normalBg);
+                chantData.datasets[0].borderColor.push(normalBorder);
+            }
         }
 
-        new Chart(testChart, {
+        new Chart(testChart1, {
             type: "bar",
-            data: {
-                datasets: [
-                    {
-                        data: chantData,
+            data: chantData,
+            options: {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: "Chart.js Bar Chart - Stacked",
                     },
-                ],
+                },
+                // responsive: true,
+                // interaction: {
+                //     intersect: false,
+                // },
+                scales: {
+                    x: {
+                        stacked: true,
+                    },
+                    y: {
+                        stacked: true,
+                    },
+                },
             },
         });
+        // end of creating chart
     });
 </script>
 
-<div id="experimental-panel" hidden>
+<div id="experimental-panel">
     <Section>
-        <canvas id="chart-test" bind:this={testChart}></canvas>
+        <canvas bind:this={testChart1}></canvas>
+        <!-- <canvas bind:this={testChart2}></canvas> -->
     </Section>
 </div>
 
 <style>
+    canvas {
+        width: 42rem;
+        height: 36rem;
+    }
 </style>
