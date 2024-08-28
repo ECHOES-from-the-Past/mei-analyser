b30ab31bffb8209efdc2af2c56f6a2bebba3d557
Antoine Phan <antoine.phan@mail.mcgill.ca>
2024-08-28 02:11:59 -0400

    Square chant chart

--- src/components/search/AnalysisChart.svelte
+++ src/components/search/AnalysisChart.svelte
@@ -2,9 +2,10 @@
     /*
      * The chart analysis produces a chart that analyse an input chant
      */
-    import { Chant } from "../../utility/components";
+    import { Chant, septenaryToPitchOctave, toSeptenary } from "../../utility/components";
     import { Chart } from "chart.js/auto";
     import { getNeumeComponentList } from "../../utility/components";
+    import { NeumeComponent } from "../../utility/components";
     import { onMount } from "svelte";
     /** @type {Chant} */
     export let chant;
@@ -12,30 +13,42 @@
     // The place for the chart
     let chart;
 
-    onMount(() => {
-        let notes = getNeumeComponentList(chant.syllables);
-        const pitchFrequency = notes.map((nc) => {
-            if (chant.notationType == "aquitanian") return nc.loc;
-            if (chant.notationType == "square")
-                return `${nc.pitch}${nc.octave}`;
-        });
+    // Colours
+    const normalBg = "rgba(125, 179, 102, 0.2)";
+    const normalBorder = "rgb(125, 179, 102)";
+    const redBg = "rgba(255, 99, 132, 0.2)",
+        rhombusBg = "rgba(55, 99, 132, 0.2)";
+    const redBorder = "rgb(255, 99, 132)",
+        rhombusBorder = "rgb(55, 99, 132)";
 
-        let counts = {};
-        // Count the frequency of each note
-        pitchFrequency.forEach((note) => {
-            if (counts[note] === undefined) {
-                counts[note] = 1;
-            } else {
-                counts[note] += 1;
-            }
-        });
+    /**
+     * Take the Aquitanian neume component list, output useable chant data for the chart
+     *
+     * @param {NeumeComponent[]} chantNC
+     */
+    function getChantDataAquitanian(chantNC) {
+        let chantData = {
+            labels: [],
+            datasets: [
+                {
+                    label: "Number of notes",
+                    data: [],
+                    backgroundColor: [],
+                    borderColor: [],
+                    borderWidth: 1,
+                },
+                {
+                    label: "Rhombus",
+                    data: [],
+                    backgroundColor: [],
+                    borderColor: [],
+                    borderWidth: 1,
+                },
+            ],
+        };
 
-        let sortedKeys = Object.keys(counts).sort((a, b) => a - b);
-
-        let sortedCounts = new Map();
-        sortedKeys.forEach((e, i, arr) => {
-            let value = sortedKeys[i];
-            sortedCounts.set(`${e}`, counts[value]);
+        const pitchList = chantNC.map((nc) => {
+            return nc.loc;
         });
 
         let noteCounts = {
@@ -43,26 +56,14 @@
             regular: {},
         };
 
-        let neumeComponents = notes;
-        neumeComponents.map((nc) => {
-            let note;
-            if (chant.notationType == "aquitanian") {
-                note = nc.loc;
-            } else if (chant.notationType == "square") {
-                note = nc.pitch;
-            }
+        chantNC.map((nc) => {
+            let note = nc.loc;
 
             if (nc.tilt == "se") {
                 if (noteCounts.rhombus[note] === undefined) {
                     noteCounts.rhombus[note] = 1;
                 } else {
                     noteCounts.rhombus[note] += 1;
-                }
-            } else {
-                if (noteCounts.regular[note] === undefined) {
-                    noteCounts.regular[note] = 1;
-                } else {
-                    noteCounts.regular[note] += 1;
                 }
             }
         });
@@ -83,15 +84,39 @@
             });
         }
 
-        const finalis = notes[notes.length - 1];
+        const finalis = chantNC[chantNC.length - 1];
 
-        const normalBg = "rgba(125, 179, 102, 0.2)";
-        const normalBorder = "rgb(125, 179, 102)";
-        const redBg = "rgba(255, 99, 132, 0.2)",
-            rhombusBg = "rgba(55, 99, 132, 0.2)";
-        const redBorder = "rgb(255, 99, 132)",
-            rhombusBorder = "rgb(55, 99, 132)";
+        // Adding rhombus
+        // TODO: Subtract the number of rhombus from the chart
+        Object.keys(sortedCountsFreq).forEach((element, i, arr) => {
+            // e is 'regular' and 'rhombus'
+            for (let key of sortedCountsFreq[element].keys()) {
+                if (!chantData.labels.includes(key)) {
+                    chantData.labels.push(`${key}`);
+                }
+                chantData.datasets[i].data.push(
+                    sortedCountsFreq[element].get(key),
+                );
+                chantData.datasets[i].backgroundColor.push(
+                    element == "regular" ? normalBg : rhombusBg,
+                );
+                chantData.datasets[i].borderColor.push(
+                    element == "regular" ? normalBorder : rhombusBorder,
+                );
+            }
+        });
 
+        chantNC.forEach((e, i, arr) => {});
+
+        return chantData;
+    }
+
+    /**
+     * Take the Square neume component list, output useable chant data for the chart
+     *
+     * @param {NeumeComponent[]} chantNC
+     */
+    function getChantDataSquare(chantNC) {
         let chantData = {
             labels: [],
             datasets: [
@@ -105,50 +130,69 @@
             ],
         };
 
-        if (chant.notationType == "aquitanian") {
-            chantData.datasets.push({
-                label: "Rhombus",
-                data: [],
-                backgroundColor: [],
-                borderColor: [],
-                borderWidth: 1,
-            });
+        let counts = {}
+        let lowestSeptenary = 99, highestSeptenary = 0, temp;
+        let pitchList = chantNC.map((nc) => {
+            temp = toSeptenary(nc);
+            if (temp > highestSeptenary) highestSeptenary = temp;
+            if (temp < lowestSeptenary) lowestSeptenary = temp;
+            return `${nc.pitch}${nc.octave}`;
+        });
+
+        console.log(septenaryToPitchOctave(lowestSeptenary), septenaryToPitchOctave(highestSeptenary))
+
+        let highestNote = septenaryToPitchOctave(highestSeptenary);
+        let lowestNote = septenaryToPitchOctave(lowestSeptenary);
+
+        for (let i = lowestSeptenary; i <= highestSeptenary; i++) {
+            let note = septenaryToPitchOctave(i);
+            counts[note] = 0;
         }
 
-        for (let key of sortedCounts.keys()) {
-            if (
-                key == finalis.loc ||
-                key == `${finalis.pitch}${finalis.octave}`
-            ) {
+        // Find the lowest note and find the highest note
+        // Construct the entire range
+        // Append count to all notes
+
+        const finalis = chantNC[chantNC.length - 1];
+
+        // Count the frequency of each note
+        pitchList.forEach((note) => {
+                counts[note] += 1;
+        });
+
+        let ambitusCount = new Map();
+
+        let sortedKeys = Object.keys(counts).sort((a, b) => a - b);
+        sortedKeys.forEach((e, i, arr) => {
+            let value = sortedKeys[i];
+            ambitusCount.set(`${e}`, counts[value]);
+        });
+
+        // chantNC.forEach((nc, i, arr) => {});
+
+        for (let key of ambitusCount.keys()) {
+            if (key == `${finalis.pitch}${finalis.octave}`) {
                 chantData.labels.push(`${key} (finalis)`);
             } else {
                 chantData.labels.push(key);
             }
-            chantData.datasets[0].data.push(sortedCounts.get(key));
+            chantData.datasets[0].data.push(ambitusCount.get(key));
             chantData.datasets[0].backgroundColor.push(normalBg);
             chantData.datasets[0].borderColor.push(normalBorder);
         }
 
-        // Adding rhombus
-        // TODO: Subtract the number of rhombus from the chart
+        return chantData;
+    }
+
+    // Basically the main function of the analysis chart
+    onMount(() => {
+        let notes = getNeumeComponentList(chant.syllables);
+        let chantData;
+
         if (chant.notationType == "aquitanian") {
-            Object.keys(sortedCountsFreq).forEach((element, i, arr) => {
-                // e is 'regular' and 'rhombus'
-                for (let key of sortedCountsFreq[element].keys()) {
-                    if (!chantData.labels.includes(key)) {
-                        chantData.labels.push(`${key}`);
-                    }
-                    chantData.datasets[i].data.push(
-                        sortedCountsFreq[element].get(key),
-                    );
-                    chantData.datasets[i].backgroundColor.push(
-                        element == "regular" ? normalBg : rhombusBg,
-                    );
-                    chantData.datasets[i].borderColor.push(
-                        element == "regular" ? normalBorder : rhombusBorder,
-                    );
-                }
-            });
+            chantData = getChantDataAquitanian(notes);
+        } else if (chant.notationType == "square") {
+            chantData = getChantDataSquare(notes);
         }
 
         new Chart(chart, {
