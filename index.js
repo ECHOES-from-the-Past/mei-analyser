cdac5077a0d71a2244407881c2c901bf36b8ee54
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-01-16 11:22:20 -0500

    Code clean up

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 import App from './src/App.svelte';
-
 import { mount } from 'svelte';
-
-const app = mount(App, { target: document.getElementById("app") });
-export default app;
+export default mount(App, { target: document.getElementById("app") });
