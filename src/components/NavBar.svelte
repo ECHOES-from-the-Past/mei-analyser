950d9acdd4bb3687b0eb14a18d10019cfb37c16d
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-04-27 16:35:31 -0400

    Version 0.6.6: PAM

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,24 +1,33 @@
 <script>
-    import { env } from "@utility/utils";
-    import Button from "./Button.svelte";
-    import ExternalLink from "./ExternalLink.svelte";
+  import { env } from "@utility/utils";
+  import Button from "./Button.svelte";
+  import ExternalLink from "./ExternalLink.svelte";
 
-    let clientVersion = $state();
-    export function setVersion(version) {
-        clientVersion = version;
-        if (env == "development") {
-            clientVersion += " (development)";
-        }
+  let clientVersion = $state();
+  export function setVersion(version) {
+    clientVersion = version;
+    if (env == "development") {
+      clientVersion += " (development)";
     }
+  }
 </script>
 
-<div id='navbar' class="flex items-center justify-between gap-3 px-2 pt-2">
-    <p>MEI Analyser</p>
-    <ExternalLink
-        href="https://github.com/ECHOES-from-the-Past/mei-analyser/wiki"
-    >
-        <Button>Project Wiki</Button>
-    </ExternalLink>
+<div id="navbar" class="flex items-center justify-between gap-3 px-2 pt-2">
+    <p class="font-sans text-emerald-900 font-bold">
+      <span class="underline">P</span>lainchant
+      <span class="underline">A</span>nalyser for <br />
+      <span class="underline">M</span>EI-encoded Neumes 🎶
+    </p>
+  <!-- <img src="/images/PAMLogo.png" alt="PAM Logo" class='max-h-20'> -->
 
-    <p id="client-version" class="text-base text-emerald-900 ml-auto">Version: {clientVersion}</p>
+  <ExternalLink href="https://github.com/ECHOES-from-the-Past/PAM/wiki">
+    <Button>Project Wiki</Button>
+  </ExternalLink>
+  <ExternalLink href="https://github.com/ECHOES-from-the-Past/PAM">
+    <Button>View source code</Button>
+  </ExternalLink>
+
+  <p id="client-version" class="text-base text-emerald-900 ml-auto">
+    Version: {clientVersion}
+  </p>
 </div>
